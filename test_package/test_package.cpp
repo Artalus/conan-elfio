92563e2a77dd27606127da9a2235a3c05c3ecc67
Artalus <artalus-mail@yandex.ru>
2018-05-09 20:00:55 +0300

    turn into an actual package

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,25 @@
-#include <cstdlib>
+#include <elfio/elfio_dump.hpp>
 #include <iostream>
 
-int main()
+using namespace ELFIO;
+
+int main( int argc, char** argv )
 {
-    std::cout << "Bincrafters\n";
-    return EXIT_SUCCESS;
+    elfio reader;
+
+    if ( !reader.load( argv[0] ) ) {
+        std::cerr << argv[0] << " is not an ELF file\n";
+        return 1;
+    }
+
+    dump::header         ( std::cout, reader );
+    dump::section_headers( std::cout, reader );
+    dump::segment_headers( std::cout, reader );
+    dump::symbol_tables  ( std::cout, reader );
+    dump::notes          ( std::cout, reader );
+    dump::dynamic_tags   ( std::cout, reader );
+    dump::section_datas  ( std::cout, reader );
+    dump::segment_datas  ( std::cout, reader );
+
+    return 0;
 }
